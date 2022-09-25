b3122666500bc3aa70301c7b3a06f31a30d45f4c
Amirul Islam Hridoy <hossainhridoy30687@gmail.com>
2022-09-25 20:47:42 +0600

    currect

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
-
+var cors = require('cors')
+var bodyParser = require('body-parser')
 const { MongoClient, ServerApiVersion } = require('mongodb');
+
+app.use(cors())
+// app.use(bodyParser.json())
+app.use(express.json())
+
 const uri = "mongodb+srv://medical_clinic:32202910@cluster0.n2etiah.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 // client.connect(err => {
@@ -12,20 +18,20 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 // });
 
 async function run() {
+  try {
     const departmentCollection = client.db("medical-clinick").collection("department");
-    try {
-          app.get('/department', async (req, res) => {
-            const query = {}
-            const cursor = departmentCollection.find(query)
-            const result = await cursor.toArray()
-            res.send(result)
-          })
+    app.get('/department', async (req, res) => {
+      const query = {}
+      const cursor = departmentCollection.find(query)
+      const result = await cursor.toArray()
+      res.send(result)
+    })
 
-    } finally {
+  } finally {
     //   await client.close(); 
-    }
   }
-  run().catch(console.dir);
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   console.log('ok')

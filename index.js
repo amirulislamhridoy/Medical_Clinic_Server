fa4be01b6a9eea165b45803f7d3fda47fd23c0ab
Amirul Islam Hridoy <hossainhridoy30687@gmail.com>
2022-09-30 14:38:54 +0600

    'test'

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = process.env.PORT || 5000
 var cors = require('cors')
 var bodyParser = require('body-parser')
 const { MongoClient, ServerApiVersion } = require('mongodb');
+var {ObjectId} = require('mongodb'); 
 
 app.use(cors())
 // app.use(bodyParser.json())
@@ -20,10 +21,34 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
   try {
     const departmentCollection = client.db("medical-clinick").collection("department");
+    const bookCollection = client.db("medical-clinick").collection("book");
     app.get('/department', async (req, res) => {
       const query = {}
       const cursor = departmentCollection.find(query)
       const result = await cursor.toArray()
+      res.send(result)
+    })
+    app.get('/allAppointments', async (req, res) => {
+      const today = req.query.today
+      const query = {date: today}
+      const allResult = await bookCollection.find({}).toArray()
+      const todayAllResult = await bookCollection.find(query).toArray()
+      res.send({allResult, todayAllResult})
+    })
+
+    // post 
+    app.post('/book', async (req, res) => {
+      const data = req.body
+      const result = await bookCollection.insertOne(data)
+      res.send(result)
+    })
+    // s
+
+    // delete
+    app.delete('/appointment/:id', async (req, res) => {
+      const id = req.params.id
+      const filter = {_id: ObjectId(id)}
+      const result = await bookCollection.deleteOne(filter)
       res.send(result)
     })
 
